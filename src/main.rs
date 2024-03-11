9cad482fe24fd4cfbea918f06edf69ea44e62af3
nampc <namphamc99@gmail.com>
2024-03-12 00:37:56 +0700

    printing hello world with VGA buffer and unsafe rust

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,27 @@ Summary:
 "never" type (!). Read more: https://doc.rust-lang.org/nightly/std/primitive.never.html
 */
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle] // no mangling to make sure the program call _main
-pub extern "C" fn _main() -> ! {
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, byte) in HELLO.iter().enumerate() {
+        // because we use raw pointers, which cannot guarantee the data pointed to is valid
+        // unsafe block tells the compiler that we're sure that the operations are valid
+        // unsafe block doesn't skip safety checks
+        unsafe {
+            // multiply by 2 because we write 2 bytes for every entry
+            *vga_buffer.offset(i as isize * 2) = *byte; // string byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // color byte
+        }
+    }
+
     loop {}
 }
-    
