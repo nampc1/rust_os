86949d80fdde9ce2a7031ea1c79cbd0d9738d928
nampc <namphamc99@gmail.com>
2024-03-18 00:01:45 +0700

    testing (in progress)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std] // https://doc.rust-lang.org/1.30.0/book/first-edition/using-rust-without-the-standard-library.html
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
 
 /*
 Summary:
@@ -20,9 +24,50 @@ fn panic(_info: &PanicInfo) -> ! {
 
 mod vga_buffer;
 
+// slice type https://doc.rust-lang.org/std/primitive.slice.html
+// trait object https://doc.rust-lang.org/1.30.0/book/first-edition/trait-objects.html
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    
+    for test in tests {
+        test();
+    }
+    
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("ok");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 #[no_mangle] // no mangling to make sure the program call _main
 pub extern "C" fn _start() -> ! {
     println!("Hello World {}", "!");
+    
+    #[cfg(test)]
+    test_main();
+    
     // vga_buffer::print_sth();
     // let vga_buffer = 0xb8000 as *mut u8;
     // 
